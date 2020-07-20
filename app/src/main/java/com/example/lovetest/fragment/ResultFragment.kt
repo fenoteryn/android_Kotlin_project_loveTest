a36ace0954070a8828964be1c2da09cb05bbc355
fenoteryn <https://github.com/fenoteryn/android_Kotlin_project_loveTest.git>
2020-07-20 15:30:20 +0900

    Test commit

--- app/src/main/java/com/example/lovetest/fragment/ResultFragment.kt
+++ app/src/main/java/com/example/lovetest/fragment/ResultFragment.kt
@@ -39,14 +39,18 @@ class ResultFragment : Fragment() {
     }
     fun setResult(option : Int){
         when(option){
-            1 -> {tv_main.text = "You are a QUITTER!"
-            tv_sub.text = "You can let the person easily."}
-            2 -> {tv_main.text = "You should focus on yourself"
-            tv_sub.text = "You become really clingy to your ex."}
-            3 -> {tv_main.text = "You should take it easy"
-                tv_sub.text = "You can do crazy things no matter what it takes."}
-            4 -> {tv_main.text = "You are pretty mature."
-                tv_sub.text = "You can easily accept the break-up."}
+            1 -> {tv_main.text = "수리수리 마수리 마술사"
+            tv_sub.text = getString(R.string.magician)
+            resultIcon.setImageResource(R.drawable.magician)}
+            2 -> {tv_main.text = "분장을 하고 저글링을 하는 삐에로"
+            tv_sub.text = getString(R.string.clown)
+                resultIcon.setImageResource(R.drawable.clown)}
+            3 -> {tv_main.text = "화려하게 줄타기와 기술을 보여주는 곡예사"
+                tv_sub.text = getString(R.string.model)
+                resultIcon.setImageResource(R.drawable.model)}
+            4 -> {tv_main.text = "사자와 호랑이를 단숨에 제압하는 맹수조련사"
+                tv_sub.text = getString(R.string.circusstunt)
+                resultIcon.setImageResource(R.drawable.circusstunt)}
         }
     }
 }
